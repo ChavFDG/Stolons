c0506e37bdd87754092524fb7474f25715411715
Chav <Chav@DESKTOP-D65EUNS>
2017-02-25 15:15:25 +0100

    jquery up to date

--- src/Stolons/wwwroot/_references.js
+++ src/Stolons/wwwroot/_references.js
@@ -9,6 +9,7 @@
 /// <reference path="js/weekbasket.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
+/// <reference path="lib/jquery-ui/jquery-ui.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
 /// <reference path="phantomjs/rasterize.js" />
