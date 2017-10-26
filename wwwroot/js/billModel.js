643bd09e8f47a8199bf5273a227cf3629575c3a9
Nicolas Michon <nicolas.michon@inria.fr>
2017-10-26 22:07:04 +0200

    Avancements sur la page de correction de livraisons

--- wwwroot/js/billModel.js
+++ wwwroot/js/billModel.js
@@ -16,6 +16,7 @@ ProducerBillModel = Backbone.Model.extend({
 	this.fetch();
     },
 
+    //return the list of unique products for this producerBill by grouping billentries by products
     getProductStocks: function() {
 	var productsIdx = {};
 
@@ -25,6 +26,19 @@ ProducerBillModel = Backbone.Model.extend({
 	return _.values(productsIdx);
     },
 
+    getProductStock: function(productStockId) {
+	var productStock;
+
+	_.forEach(this.get("BillEntries"), function(billEntry) {
+	    if (billEntry.ProductStock.get("Id") == productStockId) {
+		productStock = billEntry.ProductStock;
+		return false;
+	    }
+	});
+	return productStock;
+    },
+
+    //return the list of consumers for this producerBill by grouping billentries by consumers
     getConsumers: function() {
 	var consumersIdx = {};
 
@@ -34,12 +48,12 @@ ProducerBillModel = Backbone.Model.extend({
 	return _.values(consumersIdx);
     },
 
-    //Get consumer bill entry for a given product or nil
+    //Return consumer bill entry for a given product or nil
     getBillEntry: function(consumerId, productStockId) {
 	var billEntry = null;
 
 	_.forEach(this.get("BillEntries"), function(entry) {
-	    if (entry.ProductStock.Id == productStockId && entry.ConsumerBill.AdherentStolon.Id == consumerId) {
+	    if (entry.ProductStock.get('Id') == productStockId && entry.ConsumerBill.AdherentStolon.Id == consumerId) {
 		billEntry = entry;
 		return false;
 	    }
@@ -47,17 +61,69 @@ ProducerBillModel = Backbone.Model.extend({
 	return billEntry;
     },
 
+    //Get the total quantity for this productStock
     getProductStockTotal: function(productStockId) {
 	var totalQty = 0;
 
 	_.forEach(this.get("BillEntries"), function(entry) {
-	    if (entry.ProductStock.Id == productStockId) {
+	    if (entry.ProductStock.get('Id') == productStockId) {
 		totalQty += parseInt(entry.Quantity);
 	    }
 	});
 	return totalQty;
     },
 
+    getProductStockTotalQuantityString: function(productStockId) {
+	var totalQty = this.getProductStockTotal(productStockId);
+	var productStock = this.getProductStock(productStockId);
+
+	return this.getQuantityString(productStock.get("Product").toJSON(), totalQty);
+    },
+
+    getBillEntryQuantityString: function(billEntryId) {
+	var billEntry;
+	_.forEach(this.get("BillEntries"), function(entry) {
+	    if (entry.Id == billEntryId) {
+		billEntry = entry;
+		return false;
+	    }
+	});
+	var product = billEntry.ProductStock.get('Product').toJSON();
+	return this.getQuantityString(product, billEntry.Quantity);
+    },
+
+    getQuantityString: function(product, quantity) {
+	if (product.Type == 1) {
+	    if (quantity > 1) {
+		return  quantity + " pièce";
+	    } else {
+		return  quantity + " pièces";
+	    }
+	} else {
+	    var qty = quantity * product.QuantityStep;
+
+	    if (product.ProductUnit == 0) {
+		var strUnit = " g";
+		if (qty >= 1000) {
+		    qty /= 1000;
+		    strUnit = " Kg";
+		}
+		return qty + strUnit;
+	    } else if (product.ProductUnit == 1) {
+		var strUnit = " mL";
+		if (qty >= 1000) {
+		    qty /= 1000;
+		    strUnit = " L";
+		}
+		return qty + strUnit;
+	    }
+	}
+    },
+
+    getProductStockQuantityString: function(billEntryId) {
+	
+    },
+    
     parse: function(data) {
 	if (data && data.BillEntries) {
 	    _.forEach(data.BillEntries, function(billEntry) {
--- wwwroot/js/billsManagement.js
+++ wwwroot/js/billsManagement.js
@@ -7,6 +7,14 @@ BillsManagement.CorrectionView = Backbone.View.extend({
 
     template: _.template($("#correctionModalTemplate").html()),
 
+    events: {
+	"click .minus": "decrement",
+	"click .plus": "increment",
+	"click .decrementTotal": "decrementProductTotal",
+	"click .incrementTotal": "incrementProductTotal",
+	"click #validateCorrection": "validate"
+    },
+
     initialize: function(model) {
 	this.model = model;
 	this.render();
@@ -23,11 +31,49 @@ BillsManagement.CorrectionView = Backbone.View.extend({
     open: function() {
 	this.$el.modal({ keyboard: true, show: true });
         this.$el.on('hide.bs.modal', _.bind(this.onClose, this));
+	//Initialize changed billEntries
+	this.billEntries = {};
     },
 
     onClose: function() {
 	this.$el.off('hide.bs.modal');
         this.$el.empty();
         this.model.off("sync change", this.render, this);
+    },
+
+    decrement: function(event) {
+	var billEntryId = $(event.currentTarget).data("bill-entry-id");
+	if (billEntryId) {
+	    
+	    event.preventDefault();
+	    return false;
+	}
+    },
+
+    increment: function(event) {
+	var billEntryId = $(event.currentTarget).data("bill-entry-id");
+	if (billEntryId) {
+	    console.log("Increment, bill entry id = " + billEntryId);
+	    event.preventDefault();
+	    return false;
+	}
+    },
+
+    decrementProductTotal: function(event) {
+	var productStockId = $(event.currentTarget).data("product-stock-id");
+	if (productStockId) {
+	    console.log("decrement product total:" + productStockId);
+	    event.preventDefault();
+	    return false;
+	}
+    },
+
+    incrementProductTotal: function(event) {
+	var productStockId = $(event.currentTarget).data("product-stock-id");
+	if (productStockId) {
+	    console.log("increment product total:" + productStockId);
+	    event.preventDefault();
+	    return false;
+	}
     }
 });
