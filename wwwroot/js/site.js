ed4b538ed51c1ae820f79498e7ecc7351dcdfc6e
Skylsmoi <come.huguies@gmail.com>
2017-11-12 19:02:00 +0100

    [#21] ajout de l'icon superadmin sur liste des adherents + warning-container est maintenant générique

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,21 +1,11 @@
 ﻿// Write your Javascript code.
-
-// auto adjust basket position fixed depending on scroll
-document.addEventListener("DOMContentLoaded", function () { // line equivalent to $(document).ready(() => {})
-    function updateBasketPosition() {
-        if (window.screen.width >= 990) {
-            if (window.pageYOffset >= 300) {
-                document.getElementById('tmpBasket').style.position = 'fixed'
-                document.getElementById('tmpBasket').style.top = '20px'
-            } else {
-                document.getElementById('tmpBasket').style.position = 'static'
-            }
-        } else {
-            // the line bellow is usefull to allow css rules to apply under 990px event if the js changed it
-            // js will add 'style' attribute to html that will overrides css rules by specificity
-            document.getElementById('tmpBasket').style.position = 'inherit'
-        }
-    }
-    window.onscroll = updateBasketPosition
-    window.onresize = updateBasketPosition
-}
+    
+$(document).ready(function () {
+    $('.warning-container')
+        .hover(function () {
+            $(this).children('.warning-informations').show()
+        })
+        .mouseout(function () {
+            $(this).children('.warning-informations').hide()
+        })
+});
---
+++ wwwroot/js/weekBasketScript.js
@@ -0,0 +1,19 @@
+﻿// auto adjust basket position fixed depending on scroll
+document.addEventListener("DOMContentLoaded", function () { // line equivalent to $(document).ready(() => {})
+    function updateBasketPosition() {
+        if (window.screen.width >= 990) {
+            if (window.pageYOffset >= 300) {
+                document.getElementById('tmpBasket').style.position = 'fixed'
+                document.getElementById('tmpBasket').style.top = '20px'
+            } else {
+                document.getElementById('tmpBasket').style.position = 'static'
+            }
+        } else {
+            // the line bellow is usefull to allow css rules to apply under 990px event if the js changed it
+            // js will add 'style' attribute to html that will overrides css rules by specificity
+            document.getElementById('tmpBasket').style.position = 'inherit'
+        }
+    }
+    window.onscroll = updateBasketPosition
+    window.onresize = updateBasketPosition
+})
