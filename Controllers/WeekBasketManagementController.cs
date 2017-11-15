560c43145fb3c2ec0db8b076b1bd77cafb3fee7f
Chav Efdege <chavfdg@gmail.com>
2017-11-15 23:26:39 +0100

    [#19] Ajout de UpdateBillCorrection et ajout de la VM pour Nico

--- Controllers/WeekBasketManagementController.cs
+++ Controllers/WeekBasketManagementController.cs
@@ -33,12 +33,12 @@ namespace Stolons.Controllers
         public IActionResult Index()
         {
             Stolon stolon = GetCurrentStolon();
-	        var adherentStolon = GetActiveAdherentStolon();
+            var adherentStolon = GetActiveAdherentStolon();
             VmWeekBasketManagement vm = new VmWeekBasketManagement(adherentStolon);
             vm.Stolon = GetCurrentStolon();
-            vm.ConsumerBills = _context.ConsumerBills.Include(x=>x.AdherentStolon).ThenInclude(x=>x.Adherent).Include(x=>x.AdherentStolon).ThenInclude(x=>x.Stolon).Where(x => x.State == BillState.Pending && x.AdherentStolon.StolonId == stolon.Id).OrderBy(x=>x.Adherent.Id).ToList();
+            vm.ConsumerBills = _context.ConsumerBills.Include(x => x.AdherentStolon).ThenInclude(x => x.Adherent).Include(x => x.AdherentStolon).ThenInclude(x => x.Stolon).Where(x => x.State == BillState.Pending && x.AdherentStolon.StolonId == stolon.Id).OrderBy(x => x.Adherent.Id).ToList();
             vm.ProducerBills = _context.ProducerBills.Include(x => x.AdherentStolon).ThenInclude(x => x.Adherent).Include(x => x.AdherentStolon).ThenInclude(x => x.Stolon).Where(x => x.State != BillState.Paid && x.AdherentStolon.StolonId == stolon.Id).OrderBy(x => x.Adherent.Id).ToList();
-            vm.StolonsBills = _context.StolonsBills.Include(x=>x.Stolon).Where(x => x.StolonId == stolon.Id).ToList();
+            vm.StolonsBills = _context.StolonsBills.Include(x => x.Stolon).Where(x => x.StolonId == stolon.Id).ToList();
             vm.WeekStolonsBill = vm.StolonsBills.FirstOrDefault(x => x.BillNumber.EndsWith(DateTime.Now.Year + "_" + DateTime.Now.GetIso8601WeekOfYear()));
             return View(vm);
         }
@@ -58,16 +58,16 @@ namespace Stolons.Controllers
                 "Paiement de la facture " + bill.BillNumber + " par " + bill.Adherent.Name + "( " + bill.Adherent.Id + " ) en " + EnumHelper<PaymentMode>.GetDisplayValue(paymentMode));
             //_context.Add(transaction);
             //Save
-           // _context.SaveChanges();
+            // _context.SaveChanges();
             return RedirectToAction("Index");
         }
         // GET: UpdateProducerBill
         public IActionResult UpdateProducerBill(string billNumber)
         {
-            ProducerBill bill = _context.ProducerBills.Include(x=>x.AdherentStolon).First(x => x.BillNumber == billNumber);
+            ProducerBill bill = _context.ProducerBills.Include(x => x.AdherentStolon).First(x => x.BillNumber == billNumber);
             bill.State++;
             _context.Update(bill);
-            if(bill.State == BillState.Paid)
+            if (bill.State == BillState.Paid)
             {
                 //Transaction
                 Transaction prodRefound = new Transaction(
@@ -82,7 +82,7 @@ namespace Stolons.Controllers
                     Transaction.TransactionType.Inbound,
                     Transaction.TransactionCategory.ProducersFee,
                     bill.FeeAmount,
-                    "Encaissement de la commission de la facture " + bill.BillNumber + " de " + bill.Adherent.CompanyName+ " ( " + bill.Adherent.Id + " )");
+                    "Encaissement de la commission de la facture " + bill.BillNumber + " de " + bill.Adherent.CompanyName + " ( " + bill.Adherent.Id + " )");
                 _context.Add(comitionInbound);
             }
             //Save
@@ -90,40 +90,47 @@ namespace Stolons.Controllers
             return RedirectToAction("Index");
         }
 
+        [HttpPost, ActionName("UpdateBillCorrection")]
+        public bool UpdateBillCorrection(VmBillCorrection billCorrection)
+        {
+            
+            return billCorrection != null;
+        }
+
         // GET: ShowBill
         public IActionResult ShowStolonsBill(string id)
         {
-            StolonsBill bill = _context.StolonsBills.FirstOrDefault(x=>x.BillNumber == id);
+            StolonsBill bill = _context.StolonsBills.FirstOrDefault(x => x.BillNumber == id);
             if (bill != null)
                 return View(bill);
             //Bill not found
             return View(null);
         }
 
-	[HttpGet, ActionName("ProducerBill"), Route("api/producerBill")]
+        [HttpGet, ActionName("ProducerBill"), Route("api/producerBill")]
         public string ProducerBill(string billId)
-	{
-	    //Entity framework ? Hum... ouai
+        {
+            //Entity framework ? Hum... ouai
             IBill bill = _context.ProducerBills.Include(x => x.BillEntries).ThenInclude(x => x.ConsumerBill)
-		.Include(x => x.BillEntries).ThenInclude(x => x.ProductStock).ThenInclude(x => x.Product)
-		.Include(x => x.BillEntries).ThenInclude(x => x.ConsumerBill).ThenInclude(x => x.AdherentStolon).ThenInclude(x => x.Adherent)
-		.Include(x => x.BillEntries).ThenInclude(x => x.ConsumerBill).ThenInclude(x => x.BillEntries)
-		.Include(x => x.AdherentStolon).ThenInclude(x => x.Adherent)
-		.First(x => x.BillId.ToString() == billId);
-	    return JsonConvert.SerializeObject(bill, Formatting.Indented, new JsonSerializerSettings()
-	    {
-		ReferenceLoopHandling = ReferenceLoopHandling.Ignore
-	    });
-	}
+                                                .Include(x => x.BillEntries).ThenInclude(x => x.ProductStock).ThenInclude(x => x.Product)
+                                                .Include(x => x.BillEntries).ThenInclude(x => x.ConsumerBill).ThenInclude(x => x.AdherentStolon).ThenInclude(x => x.Adherent)
+                                                .Include(x => x.BillEntries).ThenInclude(x => x.ConsumerBill).ThenInclude(x => x.BillEntries)
+                                                .Include(x => x.AdherentStolon).ThenInclude(x => x.Adherent)
+                                                .First(x => x.BillId.ToString() == billId);
+            return JsonConvert.SerializeObject(bill, Formatting.Indented, new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+        }
 
-	// [HttpGet, ActionName("CustomersForBill"), Route("api/billCustomers")]
+        // [HttpGet, ActionName("CustomersForBill"), Route("api/billCustomers")]
         // public string AddToBasket(string billId)
-	// {
+        // {
         //     IBill bill = _context.ProducerBills.Include(x => x.BillEntries).ThenInclude(x => x.ProductStock).ThenInclude(x => x.Product).Include(x => x.AdherentStolon).Include(x => x.AdherentStolon.Adherent).Include(x => x.AdherentStolon.Stolon).First(x => x.BillId.ToString() == billId);
-	//     return JsonConvert.SerializeObject(bill, Formatting.Indented, new JsonSerializerSettings()
-	//     {
-	// 	ReferenceLoopHandling = ReferenceLoopHandling.Ignore
-	//     });
-	// }
+        //     return JsonConvert.SerializeObject(bill, Formatting.Indented, new JsonSerializerSettings()
+        //     {
+        // 	ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+        //     });
+        // }
     }
 }
---
+++ ViewModels/WeekBasketManagement/VmBillCorrection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stolons.ViewModels.WeekBasketManagement
+{
+    public class VmBillCorrection
+    {
+        public string Reason { get; set; }
+
+        public List<BillIdQuantity> NewQuantities { get; set; } = new List<BillIdQuantity>();
+    }
+
+    public class BillIdQuantity
+    {
+        public Guid BillId { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
