352036d36d366ceb93ac72b1fd6b547837b62b45
Chav Efdege <chavfdg@gmail.com>
2017-11-22 22:24:03 +0100

    Ajout d'une fonction de test de la génération d'un fichier pdf

--- Controllers/ApplicationConfigController.cs
+++ Controllers/ApplicationConfigController.cs
@@ -9,6 +9,11 @@ using Microsoft.AspNetCore.Authorization;
 using static Stolons.Configurations;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Hosting;
+using Stolons.Tools;
+using System.Net.Http;
+using System.Net;
+using System.IO;
+using System.Net.Http.Headers;
 
 //TODO tout à revoir ici. On a d'un coté la configuration global de l'application et de l'autre la configuration d'un Stolon
 
@@ -53,8 +58,18 @@ namespace Stolons.Controllers
         [ActionName("TestChromiumPath")]
         public bool TestChromiumPath()
         {
-            ApplicationConfig appConfig = _context.ApplicationConfig.First();
-            return System.IO.File.Exists(appConfig.ChromiumFullPath);
+            return System.IO.File.Exists(Application.ChromiumFullPath);
+        }
+
+        [ActionName("TestPDF")]
+        public FileResult TestPDF()
+        {
+            string tempFilePath = Path.Combine(Configurations.Environment.WebRootPath, "temp", "test.pdf");
+            BillGenerator.GeneratePDF("Test de la génération d'un PDF", tempFilePath);
+
+            byte[] fileBytes = System.IO.File.ReadAllBytes(tempFilePath);
+            return File(fileBytes, "application/pdf", "test.pdf");
+
         }
 
         // POST: Stolon/Edit/5
