e750d1d4690727e50445a00d350ca4ebddd9feb5
Nicolas Michon <nicolas.michon@inria.fr>
2018-07-04 22:16:10 +0200

    Revert "/!\ Temporaire : début de mise en place des tooltips bootstrap"
    This reverts commit 12ac3a710b13e2a94687ef18a37687908795d0fe.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,13 +1,11 @@
 ﻿// Write your Javascript code.
     
 $(document).ready(function () {
-    //$('.warning-container')
-    //    .hover(function () {
-    //        $(this).children('.warning-informations').show()
-    //    })
-    //    .mouseout(function () {
-    //        $(this).children('.warning-informations').hide()
-    //    })
-    $("body").tooltip({ selector: '[data-toggle=tooltip]' });
+    $('.warning-container')
+        .hover(function () {
+            $(this).children('.warning-informations').show()
+        })
+        .mouseout(function () {
+            $(this).children('.warning-informations').hide()
+        })
 });
-
