9a748244e7ce6277a4d45071146f517bc75c9aa2
Chav Efdege <chavfdg@gmail.com>
2018-07-15 18:07:21 +0200

    [#146] Suppressions des anciens tooltips

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,16 +2,6 @@
     
 $(document).ready(function () {
 
-    // DEPRECATED: tooltips should use the one from bootstrap.
-    // the code bellow is kept for background compatibility
-    $('.warning-container')
-        .hover(function () {
-            $(this).children('.warning-informations').show()
-        })
-        .mouseout(function () {
-            $(this).children('.warning-informations').hide()
-        })
-
     bootbox.setLocale("fr")
 
     $('[data-toggle="tooltip"]').tooltip({ container: 'body', html: true })
