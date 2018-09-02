ce8e816b526000bf9d792c075d3ec12cb5211574
Nicolas Michon <nicolas.michon@inria.fr>
2018-09-02 17:24:00 +0200

    Amélioration des menus déroulants

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,5 +4,14 @@ $(document).ready(function () {
 
     bootbox.setLocale("fr");
 
-    $('[data-toggle="tooltip"]').tooltip({ container: 'body', html: true });
+    //$('[data-toggle="tooltip"]').tooltip({ container: 'body', html: true });
+
+    // when you hover a toggle show its dropdown menu
+    $('.dropdown').hover(function() {
+	$(this).find('.dropdown-menu').stop(true, true).delay(0).fadeIn(400);
+    }, function() {
+	$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(300);
+    });
+
+    
 });
