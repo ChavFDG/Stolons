fbff7f4e884c2941f4f475a41d1a0f0f51e4a07c
Nicolas Michon <nicolas.michon@inria.fr>
2018-09-02 18:19:27 +0200

    Corrections tooltips + tooltip page panier pour produits poids variable

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,10 +1,12 @@
 ﻿// Write your Javascript code.
 
-$(document).ready(function () {
+$(function () {
 
     bootbox.setLocale("fr");
 
-    //$('[data-toggle="tooltip"]').tooltip({ container: 'body', html: true });
+    $('[data-toggle="tooltip"]').tooltip({ container: 'body', html: true });
+
+    //$(document).tooltip();
 
     // when you hover a toggle show its dropdown menu
     $('.dropdown').hover(function() {
@@ -12,6 +14,4 @@ $(document).ready(function () {
     }, function() {
 	$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(300);
     });
-
-    
 });
