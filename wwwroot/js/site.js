f39f87c11e113611844aefb9e8cedda51d70312b
Nicolas Michon <nicolas.michon@zoho.com>
2018-10-24 19:37:15 +0200

    Dropdown automatique seulement pour le menu principal + gestion des boutons dropdown désactivé

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip({ container: 'body', html: true });
 
     // when you hover a toggle show its dropdown menu
-    $('.dropdown').hover(function() {
+    $('.auto-dropdown').hover(function() {
 	$(this).find('.dropdown-menu').stop(true, true).delay(0).fadeIn(400);
     }, function() {
 	$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(300);
